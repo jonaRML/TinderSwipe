ea42aaa450c4da609cd1b22848adbbfc859ceb31
Jona <jojhan91@gmail.com>
2024-12-30 02:37:56 -0500

    terminamos el estilo de las cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let isAnimating = false;
 let pullDeltaX = 0;
+const DESICION_THRESHOLD = 110;
 
 function startDrag(event){
     if (isAnimating) return;
@@ -16,16 +17,56 @@ function startDrag(event){
 
     document.addEventListener('mouseup', onEnd);
     document.addEventListener('touchend', onEnd, {passive: true});
-}
 
-function onMove(event){
-    const eventX = event.pageX || event.touches[0].pageX;
-    pullDeltaX = eventX - startX;
+    function onMove(event){
+        const currentX = event.pageX || event.touches[0].pageX;
+        
+        pullDeltaX = currentX - startX;
 
-    if (pullDeltaX > 0) {
-        actualCard.style.transform = `translateX(${pullDeltaX}px)`;
+        const deg = pullDeltaX / 14;
+
+        actualCard.style.cursor = 'grabbing';
+        actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`;
+    }
+
+    function onEnd(){
+        document.removeEventListener('mousemove', onMove);
+        document.removeEventListener('touchmove', onMove);
+
+        document.removeEventListener('mouseup', onEnd);
+        document.removeEventListener('touchend', onEnd);
+
+       // saber si el usuario tomo una desicion
+
+       const desicionMade = Math.abs(pullDeltaX) > DESICION_THRESHOLD;
+
+       if(desicionMade){
+        const goRight = pullDeltaX >= 0;
+        const goLeft = !goRight;
+
+        //add the class acording to the desicion
+        actualCard.classList.add(goRight ? 'go-right' : 'go-left');
+        actualCard.addEventListener('transitionend', ()=>{
+            actualCard.remove();
+        });
+       }else{
+        actualCard.classList.add('reset');
+        actualCard.classList.remove('go-right', 'go-left');
+       }
+
+       //reset the variables
+       actualCard.addEventListener('transitionend', ()=>{
+            actualCard.removeAttribute('style');
+            actualCard.classList.remove('reset');
+
+            pullDeltaX = 0;
+            isAnimating = false;
+       });
+
     }
 }
+
+
 
 
 document.addEventListener('mousedown', startDrag);
